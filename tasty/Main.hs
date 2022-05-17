c94f0b436d0b2a098eb2ce4dfe4c1385a0d8dc58
Arnau Abella <arnauabella@gmail.com>
2022-05-17 22:02:11 +0200

    Fix hie.yaml and add tasty-smallcheck

--- tasty/Main.hs
+++ tasty/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
+import qualified Data.List as List
 import System.IO (hSetEncoding, stderr, stdout, utf8)
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.SmallCheck as SC
 
 main :: IO ()
 main = do
@@ -10,10 +12,23 @@ main = do
   hSetEncoding stderr utf8
   defaultMain tests
 
-tests :: TestTree
+tests, properties, unitTests, scProps :: TestTree
 tests =
   testGroup
     "Tests"
+    [ properties,
+      unitTests
+    ]
+properties = testGroup "Properties" [scProps]
+scProps =
+  testGroup
+    "(Checked by SmallCheck)"
+    [ SC.testProperty "sort == sort . reverse" $
+        \list -> List.sort (list :: [Int]) == List.sort (reverse list)
+    ]
+unitTests =
+  testGroup
+    "Unit tests"
     [ testCase "2+2=4" $
-        2 + 2 @?= 4
+        2 + 2 @?= (4 :: Integer)
     ]
