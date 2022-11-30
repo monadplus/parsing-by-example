af3df0598c0332bd5eb4da54a0387bc25e7e74e9
Arnau Abella <arnauabella@gmail.com>
2022-11-30 23:39:06 +0100

    TOML: Date&Time parser

--- tasty/Main.hs
+++ tasty/Main.hs
@@ -3,19 +3,19 @@
 
 module Main (main) where
 
-import Data.Function (on)
 import Data.Functor (void)
+import qualified Data.List as List
 import qualified Data.Text as Text
 import qualified Data.Time.Calendar.OrdinalDate as T
 import qualified Data.Time.LocalTime as T
 import Hedgehog (Gen, Property, forAll, property, (===))
 import qualified Hedgehog as Hedgehog
 import JSON.Class (JValue)
-import qualified JSON.Gen as Gen
-import qualified JSON.Lexer as Lexer
-import qualified JSON.Parser as Parser
+import qualified JSON.Gen as JSON.Gen
+import qualified JSON.Lexer as JSON.Lexer
+import qualified JSON.Parser as JSON.Parser
 import System.IO (hSetEncoding, stderr, stdout, utf8)
-import qualified TOML.Lexer as TOML
+import qualified TOML.Lexer as TOML.Lexer
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.Hedgehog (testPropertyNamed)
@@ -49,36 +49,44 @@ unitTests =
         "Parse ZonedTime"
         [ testCase "Date 1" $ do
             let date = "1979-05-27T07:32:00Z"
+                msg = Text.unpack date
                 expected =
-                  T.ZonedTime
-                    (T.LocalTime (T.YearDay 1979 147) (T.TimeOfDay 7 32 0))
-                    (T.hoursToTimeZone 0)
-                Just result = TOML.parseZonedTime date
-            assertEqualZonedTime date expected result,
+                  Right . List.singleton . TOML.Lexer.TimeLit . TOML.Lexer.ZonedTime $
+                    T.ZonedTime
+                      (T.LocalTime (T.YearDay 1979 147) (T.TimeOfDay 7 32 0))
+                      (T.hoursToTimeZone 0)
+                result = TOML.Lexer.lex "" date
+            assertEqual msg expected result,
           testCase "Date 2" $ do
             let date = "1979-05-27T00:32:00-07:00"
+                msg = Text.unpack date
                 expected =
-                  T.ZonedTime
-                    (T.LocalTime (T.YearDay 1979 147) (T.TimeOfDay 0 32 0))
-                    (T.hoursToTimeZone (-7))
-                Just result = TOML.parseZonedTime date
-            assertEqualZonedTime date expected result,
+                  Right . List.singleton . TOML.Lexer.TimeLit . TOML.Lexer.ZonedTime $
+                    T.ZonedTime
+                      (T.LocalTime (T.YearDay 1979 147) (T.TimeOfDay 0 32 0))
+                      (T.hoursToTimeZone (-7))
+                result = TOML.Lexer.lex "" date
+            assertEqual msg expected result,
           testCase "Date 3" $ do
             let date = "1979-05-27T00:32:00.999999-07:00"
+                msg = Text.unpack date
                 expected =
-                  T.ZonedTime
-                    (T.LocalTime (T.YearDay 1979 147) (T.TimeOfDay 0 32 00.999999))
-                    (T.hoursToTimeZone (-7))
-                Just result = TOML.parseZonedTime date
-            assertEqualZonedTime date expected result,
+                  Right . List.singleton . TOML.Lexer.TimeLit . TOML.Lexer.ZonedTime $
+                    T.ZonedTime
+                      (T.LocalTime (T.YearDay 1979 147) (T.TimeOfDay 0 32 00.999999))
+                      (T.hoursToTimeZone (-7))
+                result = TOML.Lexer.lex "" date
+            assertEqual msg expected result,
           testCase "Date 4" $ do
             let date = "1979-05-27 07:32:00Z"
+                msg = Text.unpack date
                 expected =
-                  T.ZonedTime
-                    (T.LocalTime (T.YearDay 1979 147) (T.TimeOfDay 7 32 0))
-                    (T.hoursToTimeZone 0)
-                Just result = TOML.parseZonedTime date
-            assertEqualZonedTime date expected result
+                  Right . List.singleton . TOML.Lexer.TimeLit . TOML.Lexer.ZonedTime $
+                    T.ZonedTime
+                      (T.LocalTime (T.YearDay 1979 147) (T.TimeOfDay 7 32 0))
+                      (T.hoursToTimeZone 0)
+                result = TOML.Lexer.lex "" date
+            assertEqual msg expected result
         ]
 
     parseLocalTimeTests =
@@ -86,16 +94,20 @@ unitTests =
         "Parse LocalTime"
         [ testCase "Date 1" $ do
             let date = "1979-05-27T07:32:00"
+                msg = Text.unpack date
                 expected =
-                  Just $
+                  Right . List.singleton . TOML.Lexer.TimeLit . TOML.Lexer.LocalTime $
                     T.LocalTime (T.YearDay 1979 147) (T.TimeOfDay 7 32 0)
-            assertEqual date expected (TOML.parseLocalTime date),
+                result = TOML.Lexer.lex "" date
+            assertEqual msg expected result,
           testCase "Date 2" $ do
             let date = "1979-05-27T00:32:00.999999"
+                msg = Text.unpack date
                 expected =
-                  Just $
+                  Right . List.singleton . TOML.Lexer.TimeLit . TOML.Lexer.LocalTime $
                     T.LocalTime (T.YearDay 1979 147) (T.TimeOfDay 0 32 00.999999)
-            assertEqual date expected (TOML.parseLocalTime date)
+                result = TOML.Lexer.lex "" date
+            assertEqual msg expected result
         ]
 
     parseTimeOfDayTests =
@@ -103,12 +115,20 @@ unitTests =
         "Parse TimeOfDay"
         [ testCase "Date 1" $ do
             let date = "07:32:00"
-                expected = Just $ T.TimeOfDay 7 32 0
-            assertEqual date expected (TOML.parseTimeOfDay date),
+                msg = Text.unpack date
+                expected =
+                  Right . List.singleton . TOML.Lexer.TimeLit . TOML.Lexer.TimeOfDay $
+                    T.TimeOfDay 7 32 0
+                result = TOML.Lexer.lex "" date
+            assertEqual msg expected result,
           testCase "Date 2" $ do
             let date = "00:32:00.999999"
-                expected = Just $ T.TimeOfDay 0 32 00.999999
-            assertEqual date expected (TOML.parseTimeOfDay date)
+                msg = Text.unpack date
+                expected =
+                  Right . List.singleton . TOML.Lexer.TimeLit . TOML.Lexer.TimeOfDay $
+                    T.TimeOfDay 0 32 00.999999
+                result = TOML.Lexer.lex "" date
+            assertEqual msg expected result
         ]
 
     parseDayTests =
@@ -116,12 +136,13 @@ unitTests =
         "Parse Day"
         [ testCase "Date 1" $ do
             let date = "1979-05-27"
-                expected = Just $ T.YearDay 1979 147
-            assertEqual date expected (TOML.parseDay date)
+                msg = Text.unpack date
+                expected =
+                  Right . List.singleton . TOML.Lexer.TimeLit . TOML.Lexer.Day $
+                    T.YearDay 1979 147
+                result = TOML.Lexer.lex "" date
+            assertEqual msg expected result
         ]
-
-assertEqualZonedTime :: String -> T.ZonedTime -> T.ZonedTime -> Assertion
-assertEqualZonedTime date = assertEqual date `on` T.zonedTimeToUTC
 
 propTests :: TestTree
 propTests =
@@ -151,41 +172,41 @@ propTests =
         ]
     ]
   where
-    prop_lexer_jstring = prop_lexer_generic Gen.jStringGen
-    prop_lexer_jnumber = prop_lexer_generic Gen.jNumberGen
-    prop_lexer_jarray = prop_lexer_generic Gen.jArrayGen
-    prop_lexer_jarray_ws = prop_lexer_stringify Gen.jArrayGen
-    prop_lexer_jobject = prop_lexer_generic Gen.jObjectGen
-    prop_lexer_jobject_ws = prop_lexer_stringify Gen.jObjectGen
-    prop_lexer_jvalue = prop_lexer_generic Gen.jValueGen
-    prop_lexer_jvalue_ws = prop_lexer_stringify Gen.jValueGen
+    prop_lexer_jstring = prop_lexer_generic JSON.Gen.jStringGen
+    prop_lexer_jnumber = prop_lexer_generic JSON.Gen.jNumberGen
+    prop_lexer_jarray = prop_lexer_generic JSON.Gen.jArrayGen
+    prop_lexer_jarray_ws = prop_lexer_stringify JSON.Gen.jArrayGen
+    prop_lexer_jobject = prop_lexer_generic JSON.Gen.jObjectGen
+    prop_lexer_jobject_ws = prop_lexer_stringify JSON.Gen.jObjectGen
+    prop_lexer_jvalue = prop_lexer_generic JSON.Gen.jValueGen
+    prop_lexer_jvalue_ws = prop_lexer_stringify JSON.Gen.jValueGen
 
-    prop_parser_jstring = prop_parser_generic Gen.jStringGen
-    prop_parser_jnumber = prop_parser_generic Gen.jNumberGen
-    prop_parser_jarray = prop_parser_generic Gen.jArrayGen
-    prop_parser_jarray_ws = prop_parser_stringify Gen.jArrayGen
-    prop_parser_jobject = prop_parser_generic Gen.jObjectGen
-    prop_parser_jobject_ws = prop_parser_stringify Gen.jObjectGen
-    prop_parser_jvalue = prop_parser_generic Gen.jValueGen
-    prop_parser_jvalue_ws = prop_parser_stringify Gen.jValueGen
+    prop_parser_jstring = prop_parser_generic JSON.Gen.jStringGen
+    prop_parser_jnumber = prop_parser_generic JSON.Gen.jNumberGen
+    prop_parser_jarray = prop_parser_generic JSON.Gen.jArrayGen
+    prop_parser_jarray_ws = prop_parser_stringify JSON.Gen.jArrayGen
+    prop_parser_jobject = prop_parser_generic JSON.Gen.jObjectGen
+    prop_parser_jobject_ws = prop_parser_stringify JSON.Gen.jObjectGen
+    prop_parser_jvalue = prop_parser_generic JSON.Gen.jValueGen
+    prop_parser_jvalue_ws = prop_parser_stringify JSON.Gen.jValueGen
 
 prop_lexer_generic :: Gen JValue -> Property
 prop_lexer_generic gen = property $ do
   jValue <- forAll gen
-  let r = Lexer.lex "" (Text.pack $ show jValue)
+  let r = JSON.Lexer.lex "" (Text.pack $ show jValue)
   void $ Hedgehog.evalEither $ r
 
 prop_lexer_stringify :: Gen JValue -> Property
 prop_lexer_stringify gen = property $ do
   jValue <- forAll gen
-  stringified <- forAll (Gen.stringify jValue)
-  let r = Lexer.lex "" stringified
+  stringified <- forAll (JSON.Gen.stringify jValue)
+  let r = JSON.Lexer.lex "" stringified
   void $ Hedgehog.evalEither $ r
 
 prop_parser_generic :: Gen JValue -> Property
 prop_parser_generic gen = property $ do
   jValue <- forAll gen
-  case Parser.parse "" (Text.pack $ show jValue) of
+  case JSON.Parser.parse "" (Text.pack $ show jValue) of
     Left err -> do
       Hedgehog.annotate err
       Hedgehog.failure
@@ -194,8 +215,8 @@ prop_parser_generic gen = property $ do
 prop_parser_stringify :: Gen JValue -> Property
 prop_parser_stringify gen = property $ do
   jValue <- forAll gen
-  stringified <- forAll (Gen.stringify jValue)
-  case Parser.parse "" stringified of
+  stringified <- forAll (JSON.Gen.stringify jValue)
+  case JSON.Parser.parse "" stringified of
     Left err -> do
       Hedgehog.annotate err
       Hedgehog.failure
